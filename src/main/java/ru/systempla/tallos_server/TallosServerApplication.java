47ce4fc80c5c54e3f6580d51e0fbf5a12382b17b
SystemPLA <amalovatyj@yandex.ru>
2020-01-27 12:53:18 +0300

    initial commit

---
+++ src/main/java/ru/systempla/tallos_server/TallosServerApplication.java
@@ -0,0 +1,13 @@
+package ru.systempla.tallos_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallosServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallosServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/systempla/tallos_server/TallosServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.systempla.tallos_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallosServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
